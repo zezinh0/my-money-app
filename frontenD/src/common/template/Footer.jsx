14b280f71a5e354b07cce04db61b128055d7683f
zezinh0 <ze_manuel_costa1998@hotmail.com>
2025-01-10 17:19:59 +0000

    Initial structure of the website and Redux

---
+++ frontenD/src/common/template/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default propps => (
+    <footer className="main-footer">
+        <strong>
+            CopyRight &copy; 2017
+        </strong>
+    </footer>
+)
---
+++ frontenD/src/dashboard/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import ContentHeader from "../common/template/ContentHeader";
+import Content from "../common/template/Content";
+import ValueBox from "../common/widget/ValueBox";
+import Row from "../common/layout/Row";
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='$ 10' text='Total de Créditos'/>
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='$ 10' text='Total de Débitos'/>
+                        <ValueBox cols='12 4' color='blue' icon='money' value='$ 0' text='Valor Consolidado'/>
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
+/*export default props => (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+)*/
